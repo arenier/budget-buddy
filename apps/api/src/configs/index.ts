2227b49d7128845ab71a969e8b521256692ffd04
Adrien RENIER <a.renier28@gmail.com>
2025-09-01 23:42:08 +0200

    chore: improve typing and logging in bootstrap

--- apps/api/src/configs/index.ts
+++ apps/api/src/configs/index.ts
@@ -1,4 +1,13 @@
-import { appConfig } from './app.config';
-import { databaseConfig } from './database.config';
+import type { ConfigService } from '@nestjs/config';
+import { appConfig, type IAppConfig } from './app.config';
+import { databaseConfig, type IDatabaseConfig } from './database.config';
 
 export const configurations = [appConfig, databaseConfig];
+
+export type IConfigService = ConfigService<
+  {
+    app: IAppConfig;
+    database: IDatabaseConfig;
+  },
+  true
+>;
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,27 +2,19 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
-// import { AppEnvDto } from './dtos/app.env.dto';
+import type { IConfigService } from './configs';
+import type { IAppConfig } from './configs/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 
-  // Typage du ConfigService pour AppEnvDto
-  const configService =
-    app.get<
-      ConfigService<
-        {
-          'app.globalPrefix': string;
-        },
-        true
-      >
-    >(ConfigService);
-  const globalPrefix = configService.get<string>('app.globalPrefix');
-  // Logger.log(`Application is running on: http://localhost:${appConfig.port}`);
+  const configService = app.get<IConfigService>(ConfigService);
+  const appConfig = configService.get<IAppConfig>('app');
 
-  app.setGlobalPrefix(globalPrefix);
+  Logger.log(`Application running on: http://localhost:${appConfig.port}`);
+
+  app.setGlobalPrefix(appConfig.globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
 }
 bootstrap();
